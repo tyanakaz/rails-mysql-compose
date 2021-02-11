b9539b0643dd6c0ff88eac17766563812ffebb38
tyanakaz <xe314m@gmail.com>
2021-02-11 22:22:25 +0900

    Setting mysql

--- rails-compose.sh
+++ rails-compose.sh
@@ -8,15 +8,19 @@ touch Gemfile.lock
 # docker-compose run api rails new . --database=mysql
 
 # API
-docker-compose run api rails new . --database=mysql --api 
-
-sleep 5 
-
-docker-compose build --force-rm
+docker-compose run api rails new . --database=mysql --api
 
 sleep 5
 
-sed -ie 's/password:/password: password/g' config/database.yml 
+docker-compose build
+
+sleep 5
+
+docker-compose exec db mysql -hdb -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';"
+
+aleep 5
+
+sed -ie 's/password:/password: password/g' config/database.yml
 sed -ie 's/host: localhost/host: db/g' config/database.yml
 
 docker-compose up -d && docker-compose exec api rake db:create
