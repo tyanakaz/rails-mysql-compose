b596acf2cce72e4545e6471f181f9d263f5fdd73
tyanakaz <xe314m@gmail.com>
2022-02-25 01:49:01 +0900

    Fixed rails compose script

--- rails-compose.sh
+++ rails-compose.sh
@@ -4,17 +4,14 @@ touch .env
 
 touch Gemfile.lock
 
+docker-compose build
+
 # Full stack
 # docker-compose run api rails new . --database=mysql
 
 # API
 docker-compose run api rails new . --database=mysql --api
-
-sleep 5
-
 docker-compose build
-
-sleep 5
 
 sed -ie 's/password:/password: password/g' config/database.yml
 sed -ie 's/host: localhost/host: db/g' config/database.yml
