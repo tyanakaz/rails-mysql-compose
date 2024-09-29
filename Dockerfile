dca8a92432783c9236513e348314855af13a2d08
tyanakaz <xe314m@gmail.com>
2024-09-29 18:34:34 +0900

    環境の準備が完了

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,16 @@
-FROM ruby:3.2.1-buster
-RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
-RUN mkdir /usr/src/app
-WORKDIR /usr/src/app
-ADD Gemfile /usr/src/app/Gemfile
-ADD Gemfile.lock /usr/src/app/Gemfile.lock
-RUN bundle install
-ADD . /usr/src/app
+# Make sure RUBY_VERSION matches the Ruby version in .ruby-version
+FROM ruby:3.3.5-slim
 
+RUN apt-get update -qq && apt-get install -y build-essential ruby-dev libmariadb-dev-compat libmariadb-dev
+
+WORKDIR /app
+
+# GemfileとGemfile.lockをコンテナにコピー
+COPY Gemfile /app/Gemfile
+COPY Gemfile.lock /app/Gemfile.lock
+
+# Bundlerを使用してGemをインストール
+RUN bundle install
+
+# アプリケーションの残りの部分をコンテナにコピー
+COPY . /app
--- rails-compose.sh
+++ rails-compose.sh
@@ -1,28 +1,11 @@
 #!/bin/sh
 
-touch .env
-
-touch Gemfile.lock
-
-docker-compose build
-
-# Full stack
-# docker-compose run api rails new . --database=mysql
-
-# API
-docker-compose run api rails new . --database=mysql --api
-docker-compose build
-
-sed -ie 's/password:/password: password/g' config/database.yml
-sed -ie 's/host: localhost/host: db/g' config/database.yml
-sed -ie 's/config.file_watcher = ActiveSupport::EventedFileUpdateChecker/# config.file_watcher = ActiveSupport::EventedFileUpdateChecker/g' config/environments/development.rb
-
-docker-compose up -d
-
-sleep 15
-
-docker-compose exec db mysql -hdb -e "ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY 'password';"
-docker-compose exec api rake db:create
-
-echo "# Mysql DB Volumes" >> .gitignore
-echo "mysql/volumes/*" >> .gitignore
+rails new .  \
+--devcontainer
+--database=mysql  \
+--skip-action-mailer \
+--skip-action-mailbox  \
+--javascript=esbuild  \
+--css=tailwind  \
+--force   \
+-m template.rb
